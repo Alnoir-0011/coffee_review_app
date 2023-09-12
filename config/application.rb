993acd092b16c28fd452d32f9ac0047bb158c2f2
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-09-13 06:55:54 +0900

    set locale timezone

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,10 @@ module Myapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
+    config.i18n.dafault_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
