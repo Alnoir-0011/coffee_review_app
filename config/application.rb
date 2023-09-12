ae9a15796c957dd6070a411e9bbfeb159dda19b0
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-09-13 07:46:54 +0900

    fix: typo

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Myapp
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
 
-    config.i18n.dafault_locale = :ja
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
