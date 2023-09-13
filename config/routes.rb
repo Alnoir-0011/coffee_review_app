750cf2728d6168cdb69118ef192d40452ff60689
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-09-13 22:33:46 +0900

    add: signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[new create]
+  root to: 'tops#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
