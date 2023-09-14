eabf347b0be0c53eb6dc1d41949cc0643cde17f5
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-09-14 22:21:24 +0900

    fix: ransackble_methods

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,7 +1,14 @@
 class Region < ApplicationRecord
-  authorizable_ransackable_attributes
-  
   has_many :beans, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+  validates :name, length: { maximum: 255 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    authorizable_ransackable_attributes
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    authorizable_ransackable_associations
+  end
 end
