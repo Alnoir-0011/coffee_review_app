750a5bc01fbcb12581bad47dffee27e0b244338b
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-09-14 22:19:55 +0900

    fix: comentcodes

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -2,11 +2,7 @@ class BeansController < ApplicationController
   def index
     # @category = Region.find_by(params[:category])
     # redirect_to root_path unless @category
-    # if @category
-    #   @q = @category.beans.ransack(params[:q])
-    # else
-    #   @q = Bean.ransack(params[:q])
-    # end
+    # @q = @category.beans.ransack(params[:q])
     @q = Bean.ransack(params[:q])
     @beans = @q.result.page(params[:page])
   end
