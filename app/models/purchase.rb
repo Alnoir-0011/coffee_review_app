4a3e7f95389bb05e60eb0778d37f4c460599af3e
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-09-15 22:11:11 +0900

    add: ransackble methods

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -9,6 +9,15 @@ class Purchase < ApplicationRecord
   enum :store_roast_option, { roasted: 0, light: 10, chinamon: 20, medium: 30, high: 40, city: 50, fullcity: 60, fremch: 70, italian: 80 }, prefix: true
   enum :store_grind_option, { grinded: 0, beans: 10, coarsely: 20, medium: 30, medium_fine: 40, fine: 50, superfine: 60 }, prefix: true
 
+
+  def self.ransackable_attributes(auth_object = nil)
+    authorizable_ransackable_attributes
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    authorizable_ransackable_associations
+  end
+  
   private
 
   def future_dates_cannot
