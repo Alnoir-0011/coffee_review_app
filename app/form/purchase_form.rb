bee4dd6959afb7d606e58c7376203be7c18b484b
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-09-17 20:49:20 +0900

    fix: reduce SQL

--- app/form/purchase_form.rb
+++ app/form/purchase_form.rb
@@ -18,8 +18,8 @@ class PurchaseForm
   validate :product_does_not_exist
   validate :shop_does_not_exist
   validate :future_dates_cannot, unless: -> { purchase_at.nil? }
-  validate :roast_status, unless: -> { Bean.find_by(name: bean_name).nil? }
-  validate :grind_status,  unless: -> { Bean.find_by(name: bean_name).nil? }
+  validate :roast_status, unless: -> { bean.nil? }
+  validate :grind_status,  unless: -> { bean.nil? }
 
   def initialize(attributes = nil, purchase: Purchase.new)
     @purchase = purchase
@@ -39,7 +39,7 @@ class PurchaseForm
     return false if invalid?
     ActiveRecord::Base.transaction do
       # binding.pry
-      @purchase.assign_attributes(user_id: user_id, shop_id: Shop.find_by(name: shop_name).id, bean_id: Bean.find_by(name: bean_name).id,
+      @purchase.assign_attributes(user_id: user_id, shop_id: shop.id, bean_id: bean.id,
                                    store_roast_option: store_roast_option, store_grind_option: store_grind_option, purchase_at: purchase_at)
 
       @purchase.save!
@@ -57,7 +57,7 @@ class PurchaseForm
     return false if invalid?
 
     ActiveRecord::Base.transaction do
-      @purchase.update!(shop_id: Shop.find_by(name: shop_name).id, bean_id: Bean.find_by(name: bean_name).id,
+      @purchase.update!(shop_id: shop.id, bean_id: bean.id,
                         store_roast_option: store_roast_option, store_grind_option: store_grind_option, purchase_at: purchase_at)
 
       unless @purchase.shop.beans.include?(@purchase.bean)
@@ -88,6 +88,14 @@ class PurchaseForm
     }
   end
 
+  def bean
+    @bean ||= Bean.find_by(name: bean_name)
+  end
+
+  def shop
+    @shop ||= Shop.find_by(name: shop_name)
+  end
+
   # def purchase
   #   @purchase ||= User.find(self.user_id).purchases.new(store_roast_option: store_roast_option,
   #                 store_grind_option: store_grind_option,purchase_at: purchase_at,
@@ -99,7 +107,7 @@ class PurchaseForm
   end
 
   def roast_status
-    bean = Bean.find_by(name: self.bean_name)
+    # bean = Bean.find_by(name: self.bean_name)
     if bean.roast_raw? && self.store_roast_option == 'roasted'
       errors.add(:store_roast_option, "このコーヒー豆は焙煎前で登録できません")
     elsif !bean.roast_raw? && !self.store_roast_option == 'roasted'
@@ -108,7 +116,7 @@ class PurchaseForm
   end
 
   def grind_status
-    bean = Bean.find_by(name: self.bean_name)
+    # bean = Bean.find_by(name: self.bean_name)
     if !bean.fineness_beans? && !self.store_grind_option == 'grinded'
       errors.add(:store_grind_option, "このコーヒー豆は粉砕済みです")
     elsif bean.fineness_beans? && self.store_grind_option == 'grinded'
@@ -117,10 +125,10 @@ class PurchaseForm
   end
 
   def product_does_not_exist
-    errors.add(:bean_name, "商品が存在しません") unless Bean.find_by(name: self.bean_name)
+    errors.add(:bean_name, "商品が存在しません") unless bean# Bean.find_by(name: self.bean_name)
   end
 
   def shop_does_not_exist
-    errors.add(:shop_name, "店舗が存在しません") unless Shop.find_by(name: self.shop_name)
+    errors.add(:shop_name, "店舗が存在しません") unless shop # Shop.find_by(name: self.shop_name)
   end
 end
