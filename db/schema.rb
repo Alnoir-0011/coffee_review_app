b42ee042bc81a129f909f703daadb2d2dc7c3ba0
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-09-18 09:04:09 +0900

    add: admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_17_170736) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_17_231620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "administrators", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "remember_token"
+    t.datetime "remember_token_expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "beans", force: :cascade do |t|
     t.string "name", null: false
