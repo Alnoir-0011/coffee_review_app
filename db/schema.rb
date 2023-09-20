f02768cf17260d9607420b764b1cb1226f66cedc
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-09-20 23:45:46 +0900

    add: avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_17_231620) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_135522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -139,6 +139,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_17_231620) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
