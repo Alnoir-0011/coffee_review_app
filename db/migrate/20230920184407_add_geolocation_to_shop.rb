455b2eb5968e1c6969f547f62949e702f41a86f8
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-10-11 13:46:01 +0900

    fix: phone_number column

--- db/migrate/20230920184407_add_geolocation_to_shop.rb
+++ db/migrate/20230920184407_add_geolocation_to_shop.rb
@@ -4,5 +4,7 @@ class AddGeolocationToShop < ActiveRecord::Migration[7.0]
     add_column :shops, :latitude, :float
     add_column :shops, :longitude, :float
     rename_column :shops, :adress, :address
+    remove_column :shops, :telephone_number, :integer
+    add_column :shops, :phone_number, :string
   end
 end
