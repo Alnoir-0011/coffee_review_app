0774b49d2afe452fe91849b996e0ba35808bc017
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2024-01-05 15:40:35 +0900

    add: admin authenticate

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,6 @@
 class Admin::BaseController < ApplicationController
-  # before_action :require_login
+  before_action :require_login
+  before_action :admin_authenticate
   layout 'admin/layouts/application'
 
   def index
@@ -10,5 +11,9 @@ class Admin::BaseController < ApplicationController
 
   def not_authenticated
     redirect_to admin_login_path, danger: 'require login'
+  end
+
+  def admin_authenticate
+    redirect_to root_path unless current_user.admin?
   end
 end
