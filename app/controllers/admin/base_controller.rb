55f9da64f564ecaa040909d4cf80617ac4e8a0db
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2024-01-31 02:49:45 +0900

    add: meta_tags to admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,15 +3,10 @@ class Admin::BaseController < ApplicationController
   before_action :admin_authenticate
   layout 'admin/layouts/application'
 
-  def index
-    @most_reviewed_shop = Shop.joins(:reviews).group("shops.name").order('count_all DESC').limit(1).count.to_a[0]
-    @monthly_reviewed_shop = Shop.joins(:reviews).where("shops.created_at > ?", Time.current.prev_month).group("shops.name").order('count_all DESC').limit(1).count.to_a[0]
-  end
-
   private
 
   def not_authenticated
-    redirect_to admin_login_path, danger: 'require login'
+    redirect_to admin_login_path, danger: t('default.require_login')
   end
 
   def admin_authenticate
