5b408cb5092de9a4a53162a649fb6c8ac904fdf9
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2024-04-05 21:43:03 +0900

    add: google map link, place_details concern

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ApplicationRecord
+  include PlaceDetails
+
   geocoded_by latitude: :latitude, longitude: :longitude
 
   has_many :dealers, dependent: :destroy
@@ -12,11 +14,12 @@ class Shop < ApplicationRecord
   validates :place_id, length: { maximum: 255 }
   validates :address, presence: true
   validates :address, length: { maximum: 255 }
-  validates :phone_number, length: { maximum: 15 }, if: -> { phone_number }
   validates :latitude, presence: true
   validates :latitude, numericality: { in: 20..46 }
   validates :longitude, presence: true
   validates :longitude, numericality: { in: 120..150 }
+  validates :google_map_uri, presence: true, uniqueness: true
+  validates :google_map_uri, length: { maximum: 255 }
 
   def self.ransackable_attributes(auth_object = nil)
     auth_object&.admin? ? super : %w(name place_id)
