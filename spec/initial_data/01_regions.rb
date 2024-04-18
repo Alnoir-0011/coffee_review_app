1acd7e8195888705cc386cc8402593fb52ecbf1e
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2024-04-18 17:07:39 +0900

    add: rspec seeds

---
+++ spec/initial_data/01_regions.rb
@@ -0,0 +1,49 @@
+Region.seed do |s|
+  s.id = 1
+  s.name = 'ブラジル'
+end
+
+Region.seed do |s|
+  s.id = 2
+  s.name = 'コロンビア'
+end
+
+Region.seed do |s|
+  s.id = 3
+  s.name = 'グアテマラ'
+end
+
+Region.seed do |s|
+  s.id = 4
+  s.name = 'マンデリン'
+end
+
+Region.seed do |s|
+  s.id = 5
+  s.name = 'コナ'
+end
+
+Region.seed do |s|
+  s.id = 6
+  s.name = 'キリマンジャロ'
+end
+
+Region.seed do |s|
+  s.id = 7
+  s.name = 'モカ'
+end
+
+Region.seed do |s|
+  s.id = 8
+  s.name = 'ブルーマウンテン'
+end
+
+Region.seed do |s|
+  s.id = 9
+  s.name = 'ケニア'
+end
+
+Region.seed do |s|
+  s.id = 10
+  s.name = 'その他'
+end
