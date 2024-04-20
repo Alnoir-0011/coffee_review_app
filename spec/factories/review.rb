42dc28d37344f802cf01c52cffd29aa420150f1d
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2024-04-20 17:17:42 +0900

    runned rubocop

--- spec/factories/review.rb
+++ spec/factories/review.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     sequence(:title) { |n| "title_#{n}" }
     fineness { 'medium' }
     evaluation { 3 }
-    content { 'test_review_content'}
+    content { 'test_review_content' }
     brewing_method_id { 1 }
     purchase
     image { File.open(Rails.root.join('public/images/noimage.jpg').to_s) }
