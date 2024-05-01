341ee9449df762b11576f6ca07730f02d1e6780e
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2024-05-01 09:39:18 +0900

    js application debug false

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,7 +5,7 @@ const application = Application.start()
 application.register('autocomplete', Autocomplete)
 
 // Configure Stimulus development experience
-application.debug = true
+application.debug = false
 window.Stimulus   = application
 
 export { application }
